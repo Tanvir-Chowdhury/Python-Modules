391f8af4eec0b0e4099258ac07fe6beedd93282e
Tanvir Chowdhury <63849309+Tanvir-Chowdhury@users.noreply.github.com>
2020-06-12 16:46:12 +0600

    nothing changed

--- Collections_module.py
+++ Collections_module.py
@@ -1,4 +1,7 @@
+# Counter
 from collections import Counter as counter
+from collections import defaultdict as dd
+
 a = "Tanvir Chowdhury"
 b = counter(a)
 print(b)
@@ -9,3 +12,23 @@ for i in b.elements():
     print(f"{i} : {b[i]}")
 
 print(b.most_common(2))
+
+
+# defaultDict
+d = dd(lambda : 6644)
+d['Tanvir'] = 6465
+d['Mahi']
+print(d['Mahi'])
+print(d)
+print(d.__missing__("Mahi"))
+
+d1 = dd()
+for i,j in [("Tanvir", 11744), ("Saquib", 11738)]:
+    d1[i] = j
+
+print(d1)
+
+
+# orderedDict
+
+    

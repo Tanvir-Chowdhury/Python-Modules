3801cf73ea1840867f60d25e538a1d31c3231022
Tanvir-Chowdhury <tanvirvlogger@gmail.com>
2020-09-14 19:59:02 +0600

    added collection

--- Collections_module.py
+++ Collections_module.py
@@ -1,6 +1,9 @@
 # Counter
+
 from collections import Counter as counter
 from collections import defaultdict as dd
+from collections import OrderedDict as od
+from collections import namedtuple as nt
 
 a = "Tanvir Chowdhury"
 b = counter(a)
@@ -13,8 +16,8 @@ for i in b.elements():
 
 print(b.most_common(2))
 
-
 # defaultDict
+
 d = dd(lambda : 6644)
 d['Tanvir'] = 6465
 d['Mahi']
@@ -28,7 +31,31 @@ for i,j in [("Tanvir", 11744), ("Saquib", 11738)]:
 
 print(d1)
 
-
 # orderedDict
 
-    
+o = od()
+o["c"] = 3
+o["a"] = 1
+o["b"] = 2
+
+print(o)
+o.move_to_end("c")
+print(o)
+
+o.move_to_end("b", last = False)
+print(o)
+
+print(o.popitem())
+print(o.popitem(last=False))
+
+#NamedTuple
+
+colors = nt("colors", "r g b")
+red = colors(r = 255, g = 0, b = 255)
+print(red.r)
+print(red[0])
+print(getattr(red, "r"))
+
+print(red._asdict())
+
+print(colors._make["1","2","3"])

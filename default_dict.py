f28b54c863c915523f4053b6a052ae795e4dc8ad
Tanvir-Chowdhury <tanvirvlogger@gmail.com>
2020-09-18 17:47:48 +0600

    added dd

--- default_dict.py
+++ default_dict.py
@@ -15,3 +15,17 @@ other_dict = dd(lambda : 11744)
 other_dict["Mahi"] = 6644
 other_dict["Tanvir"] 
 print(other_dict)
+
+a = [("a", (1,2)), ("b", 2), ("c", 3)]
+b = dd(list)
+for i,j in a:
+    b[i].append(j)
+print(b)
+print(b.get("a"))
+
+name = "sabbir"
+mydict = dd(int)
+for i in name:
+    mydict[i] += 1
+print(mydict)
+

eede9e119142d1685051b8a19c89ab255c299a9e
Tanvir-Chowdhury <tanvirvlogger@gmail.com>
2020-10-02 23:39:02 +0600

    added pprint

---
+++ python_pprint.py
@@ -0,0 +1,21 @@
+import numpy as np
+from pprint import pprint
+from pprint import pformat
+import logging
+
+data = {
+    "colts" : np.random.random(10),
+    "aurrents" : np.random.random(20)
+}
+
+pprint(data)
+
+logging.basicConfig(level=logging.DEBUG,format='%(levelname)-8s %(message)s',)
+logging.debug('Logging pformatted data')
+
+formatted = pformat(data)
+
+for line in formatted.splitlines():
+    logging.debug(line.rstrip())
+
+pprint(formatted)

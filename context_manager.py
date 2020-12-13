8ae848a63d5a1c1d121f08c933b81deec8629e14
Tanvir-Chowdhiry <tanvirvlogger@gmail.com>
2020-12-13 17:07:31 +0600

    __

--- context_manager.py
+++ context_manager.py
@@ -1,6 +1,19 @@
 import os
 from contextlib import contextmanager
 
+class Context_Manager():
+    def __init__(self,filename,mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.file.close()
+
 @contextmanager
 def open_file(file, mode):
     try:
--- unit_testing.py
+++ unit_testing.py
@@ -3,6 +3,20 @@ import calc
 
 class Testcalc(unittest.TestCase):
 
+    @classmethod
+    def startUpClass(cls): # runs this function before all function
+        pass
+
+    @classmethod
+    def tearDownClass(cls): # runs this function after all function
+        pass
+
+    def startUp(self): #runs this function before every single function
+        pass
+
+    def tearDown(self): #runs this function after every single function
+        pass
+
     def test_add(self):
         self.assertEqual(calc.add(10, 5), 15)
 

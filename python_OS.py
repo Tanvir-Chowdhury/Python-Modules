483f0ec5ecb6e5fc8b0cb9b327b7f0943f4f6186
Tanvir-Chowdhiry <tanvirvlogger@gmail.com>
2020-12-15 15:58:27 +0600

    ___

--- python_OS.py
+++ python_OS.py
@@ -9,7 +9,12 @@ print(os.getcwd())
 
 print(os.popen("cp text1 text2"))  #we can os.system also
 
-print(os.listdir())'''
+print(os.listdir())
 
-print(os.path.isdir('/home/usr/local/bin/pipenv'))
-os.environ['SECRET_KEY']
+print(os.path.isdir('/home/usr/local/bin/pipenv'))'''
+
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+
+print(db_user)
+print(db_pass)

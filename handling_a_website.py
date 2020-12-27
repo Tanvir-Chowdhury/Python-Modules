f4ab36916c029a937c61829b002e26bbdc8937dd
Tanvir-Chowdhiry <tanvirvlogger@gmail.com>
2020-12-27 15:49:33 +0600

    __

--- handling_a_website.py
+++ handling_a_website.py
@@ -5,21 +5,21 @@ import requests
 EMAIL_USER = os.environ.get("EMAIL_USER")
 #Email_address = "tanvirvlogger@gmail.com"
 EMAIL_PASS = os.environ.get("EMAIL_PASS")
-#Email_pass = "TanvirTheFunkeyBoy"
+#Email_pass = 
 
 print(EMAIL_USER)
 print(EMAIL_PASS)
 
-'''r = requests.get("https://coreyms.com", timeout=5)
+r = requests.get("https://coreyms.com", timeout=5)
 if r.status_code != 200:
     with  smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.ehlo()
 
-        smtp.login(Email_address, Email_pass)
+        smtp.login(EMAIL_USER, EMAIL_PASS)
         subject = "Website down"
         body = "your website is down"
         msg = f"subject: {subject} \n\n {body}"
 
-        smtp.sendmail(Email_address, "tanvirvlogger@gmail.com", msg)'''
+        smtp.sendmail(EMAIL_USER, "tanvirvlogger@gmail.com", msg)
--- sending_email.py
+++ sending_email.py
@@ -1,3 +1,18 @@
 import smtplib
+import os
 
-with open()
+EMAIL_USER = "tanvirvlogger@gmail.com"
+EMAIL_PASS = "TanvirTheFunkeyBoy"
+
+with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_USER, EMAIL_PASS)
+
+    subject = "website down"
+    body = "your website is down"
+    msg = f"subject: {subject} \n\n {body}"
+
+    smtp.sendmail(EMAIL_USER, "tanvirchowdhury6465@gmail.com", msg)

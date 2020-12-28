7045117838b768510ce5225e21923c77cc2a803a
Tanvir-Chowdhiry <tanvirvlogger@gmail.com>
2020-12-28 17:59:05 +0600

    __

---
+++ webscrapping_html.py
@@ -0,0 +1,13 @@
+from requests_html import HTML
+
+with open('simple2.html') as html_file:
+    source = html_file.read()
+    html = HTML(html=source)
+
+    articles = html.find("div.article")
+    for article in articles:
+        headline = article.find("h2", first=True).text
+        summary = article.find("p", first=True).text
+        print(headline)
+        print(summary)
+        print()
